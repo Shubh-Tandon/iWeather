e2c045569011530a7febfd724e77b4e4e936ea5a
Shubh Tandon <imshubhtandon@gmail.com>
2023-02-19 22:22:08 +0530

    added background

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import City from './components/City';
 import Navbar from './components/Navbar';
 import SearchWeather from './components/SearchWeather';
-import WeatherPic from './image/weather3.jpeg'
+import WeatherPic from './image/weather2.jpg'
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 
 function App() {
   return (
     <>
-    <div className=' myContainer1'>
-    
+    <div className=' container myContainer1'>
       <Navbar />
+
+
       <SearchWeather />
-              
-  {/* <City /> */}
+
 
     </div>
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Navbar() {
   return (
    <>
-<nav className="navbar navbar-expand-lg  my-1 myNav">
-  <div className="container-fluid">
+<nav className="navbar navbar-expand-lg my-1 myNav mx-5 ">
+  <div className="container-fluid d-flex justify-content-center">
     <a className="navbar-brand" href="#">iWeather</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -12,11 +12,9 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        {/* <li className="nav-item">
-          <a className="nav-link" href="/">Search by City</a>
-        </li> */}
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li> 
+    
 </ul>
     
     </div>
